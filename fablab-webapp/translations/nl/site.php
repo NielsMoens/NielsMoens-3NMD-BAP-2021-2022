71aa776d1e1169ae825604c8721b6012f088e333
NielsMoens <nielmoen@student.arteveldehs.be>
2022-07-23 04:12:15 +0200

    finished the translations

---
+++ fablab-webapp/translations/nl/site.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    "Learn more" => "Lees meer",
+    "We're open today from" => "Wij zijn vandaag open van",
+    "Today's Opening Hours" => "Openingstijden van vandaag",
+    "Sorry, we’re closed today" => "Sorry, we zijn vandaag gesloten.",
+    "We'll be back open tomorrow at" => "Morgen zijn we weer open om",
+    "Closed" => "Gesloten",
+    "Address" => "Adres",
+    "Contact info" => "Contact gegevens",
+    "Take me there" => "Bereken route",
+);
